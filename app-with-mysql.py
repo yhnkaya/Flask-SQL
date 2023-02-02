00bc1ebee30297cdafb098d1e4db46e649f94824
yhnkaya <ayhanprofessional@gmail.com>
2023-02-02 15:31:18 -0500

    init

--- app-with-mysql.py
+++ app-with-mysql.py
@@ -6,7 +6,7 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 
 # Configure mysql database
-app.config['MYSQL_DATABASE_HOST'] = 'write your database endpoint'
+app.config['MYSQL_DATABASE_HOST'] = 'database-1.cspcl1n6prl.us-east-1.rds.amazonaws.com'
 app.config['MYSQL_DATABASE_USER'] = 'admin'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'Clarusway_1'
 app.config['MYSQL_DATABASE_DB'] = 'clarusway'
@@ -110,5 +110,4 @@ def add_email():
 
 # Add a statement to run the Flask application which can be reached from any host on port 80.
 if __name__ == '__main__':
-   app.run(debug=True)
-   # app.run(host='0.0.0.0', port=80)
+   app.run(host='0.0.0.0', port=80)
